de69823ffae79ebd77489f8d6249d249cc2912e9
macin <ernesto.dev24@gmail.com>
2024-11-19 23:47:20 -0600

    global filter by name tanstack table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,86 +1,113 @@
 import {
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { data } from "./data";
 
 import "./Table.scss";
-
-const columns = [
-  {
-    accessorKey: "name",
-    header: "Name",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  {
-    accessorKey: "schoolLevel",
-    header: "Nivel Academico",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  {
-    accessorKey: "subject",
-    header: "Subject",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  {
-    accessorKey: "grade",
-    header: "Grade",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  ,
-  {
-    accessorKey: "topic",
-    header: "Topic",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  {
-    accessorKey: "date",
-    header: "Date",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  {
-    accessorKey: "startClass",
-    header: "Start Class",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-  {
-    accessorKey: "endClass",
-    header: "End Class",
-    cell: (props) => <p>{props.getValue()}</p>,
-  },
-];
+import { useState } from "react";
 
 const Table = () => {
+  const columns = [
+    {
+      accessorKey: "name",
+      header: "Name",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    {
+      accessorKey: "schoolLevel",
+      header: "Nivel Academico",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    {
+      accessorKey: "subject",
+      header: "Subject",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    {
+      accessorKey: "grade",
+      header: "Grade",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    ,
+    {
+      accessorKey: "topic",
+      header: "Topic",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    {
+      accessorKey: "date",
+      header: "Date",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    {
+      accessorKey: "startClass",
+      header: "Start Class",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+    {
+      accessorKey: "endClass",
+      header: "End Class",
+      cell: (props) => <p>{props.getValue()}</p>,
+    },
+  ];
+
+  const [columnFilters, setColumnFilters] = useState([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
+    onColumnFiltersChange: setColumnFilters,
   });
 
+  const nameID = columnFilters.find((f) => f.id === "name")?.value || "";
+  const onFilterChange = (id, value) =>
+    setColumnFilters((prev) =>
+      prev
+        .filter((f) => f.id !== "name")
+        .concat({
+          id,
+          value,
+        })
+    );
+
   return (
-    <table className="table">
-      <thead className="table-header">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id} className="table-header">
-            {headerGroup.headers.map((header) => (
-              <th key={header.id}>{header.column.columnDef.header}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody className="table-body">
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <section>
+      <input
+        value={nameID}
+        type="text"
+        onChange={(e) => onFilterChange("name", e.target.value)}
+      />
+      <table className="table">
+        <thead className="table-header">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="table-header">
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>{header.column.columnDef.header}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="table-body">
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
